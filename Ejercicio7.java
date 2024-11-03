6e63cc675191f72e187a088eeef92a14e257c746
pbarlar <pbarlar@g.educaand.es>
2024-11-03 20:32:57 +0100

    Create Ejercicio7.java

---
+++ Ejercicio7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        System.out.print("Indica una cadena de caracteres: ");
+        String cadena=entrada.nextLine();
+        System.out.print("Indica el caracter de la cadena que quieres sustituir: ");
+        String caracter1=entrada.nextLine();
+        System.out.print("Indica el caracter que quieres que entre a la cadena por el sustituido: ");
+        String caracter2=entrada.nextLine();
+        cadena=cadena.replace(caracter1, caracter2);
+        System.out.println("La cadena con el caracter sustituido es: "+cadena);
+    }
+}
